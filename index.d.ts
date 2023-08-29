caf536db2f1b897adf50863268a1d2a1e824a001
Casper <casper@ext.store>
2023-08-29 16:26:11 +0200

    renamed types and added messaging

--- index.d.ts
+++ index.d.ts
@@ -9,28 +9,42 @@ declare namespace ext.runtime {
     arch: 'arm' | 'arm64' | 'x86-32' | 'x86-64'
   }
 
-  /** Extension update information. */
-  export interface ExtensionUpdate {
+  /** Update check return value. */
+  export interface UpdateCheck {
+    /** Status of the update check. */
+    status: 'no_update' | 'update_available'
+    /** Update information. */
+    details: EventUpdate | undefined
+  }
+
+  /** Update event. */
+  export interface EventUpdate {
     /** Version string. */
     version: string
     /** User friendly version name if available. */
     version_name: string | undefined
   }
 
-  /** Update check return value. */
-  export interface UpdateCheck {
-    /** Status of the update check. */
-    status: 'no_update' | 'update_available'
-    /** Update information. */
-    details: ExtensionUpdate | undefined
+  /** Permission event. */
+  export interface EventPermission {
+    /** Module ID. */
+    module: string
+    /** Permission key. */
+    permission: string
   }
 
-  /** Module state. */
-  export interface ModuleState {
+  /** Module event. */
+  export interface EventModule {
     /** Module ID. */
     id: string
     /** Module install path if available. */
     path: string | undefined
+  }
+
+  /** Message event. */
+  export interface EventMessage {
+    /** Sender extension ID. */
+    extension: string
   }
 
   /** Event handler. */
@@ -47,23 +61,48 @@ declare namespace ext.runtime {
     removeListener: (listener: Listener) => void
   }
 
-  /** Get extension manifest. */
+  /**
+   * Get extension manifest.
+   * @returns Extension manifest.
+   */
   export function getManifest(): Promise<any>
 
-  /** Get platform information. */
+  /**
+   * Get platform information.
+   * @returns Platform information.
+   */
   export function getPlatformInfo(): Promise<PlatformInfo>
 
-  /** Reload extension. */
+  /**
+   * Reload extension.
+   * The promise resolves after the reload is successfully requested.
+   */
   export function reload(): Promise<void>
 
-  /** Check for extension updates. */
+  /**
+   * Check for extension updates.
+   * @returns The promise resolves with the update check result.
+   */
   export function requestUpdateCheck(): Promise<UpdateCheck>
 
-  /** Get all loaded module ID's. */
+  /**
+   * Get all loaded module ID's.
+   * @returns The promise resolves with an array of all loaded module ID's.
+   */
   export function getModules(): Promise<string[]>
 
-  /** Get all granted permissions for all modules. */
+  /**
+   * Get all granted permissions for all modules.
+   * @returns The promise resolves with an object that contains a granted permission array for each module.
+   */
   export function getPermissions(): Promise<{[module: string]: string[]}>
+
+  /**
+   * Send a message to an extension.
+   * @param message Message object to send.
+   * @returns The promise resolves when the message is sent.
+   */
+  export function sendMessage(message: any): Promise<void>
   
   /** Called when the extension is installed. */
   export const onInstall: EventHandler<() => void>
@@ -78,21 +117,24 @@ declare namespace ext.runtime {
   export const onDisable: EventHandler<() => void>
 
   /** Called when an extension update is available. */
-  export const onUpdateAvailable: EventHandler<(details: ExtensionUpdate) => void>
+  export const onUpdateAvailable: EventHandler<(event: EventUpdate) => void>
 
   /** Called when a permission has been granted. */
-  export const onPermissionGrant: EventHandler<(moduleID: string, permissionKey: string) => void>
+  export const onPermissionGrant: EventHandler<(event: EventPermission) => void>
 
   /** Called when a permission has been revoked. */
-  export const onPermissionRevoke: EventHandler<(moduleID: string, permissionKey: string) => void>
+  export const onPermissionRevoke: EventHandler<(event: EventPermission) => void>
   
   /** Called when a module has been loaded. */
-  export const onModuleLoad: EventHandler<(module: ModuleState) => void>
+  export const onModuleLoad: EventHandler<(event: EventModule) => void>
 
   /** Called when a module has been unloaded. */
-  export const onModuleUnload: EventHandler<(module: ModuleState) => void>
+  export const onModuleUnload: EventHandler<(event: EventModule) => void>
 
   /** Called when an extension is clicked in the dashboard. */
   export const onExtensionClick: EventHandler<() => void>
 
+  /** Called when an extension message is received. */
+  export const onMessage: EventHandler<(event: EventMessage, message: any) => void>
+  
 }
